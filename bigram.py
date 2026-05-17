f15cb8f06bf79c32c8e837568454efbd724cba46
menisadi <menisadi@gmail.com>
2026-05-17 22:36:25 +0300

    added: starting to work on nn bigram model

--- bigram.py
+++ bigram.py
@@ -1,5 +1,12 @@
-def load_data():
-    pass
+import csv
+
+
+def load_data(file_name: str):
+    with open(file_name, newline="") as f:
+        reader = csv.reader(f)
+        first_column = [row[0] for row in reader]
+
+    return first_column[1:]
 
 
 def bigram_counts():
